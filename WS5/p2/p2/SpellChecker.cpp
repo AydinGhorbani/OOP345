45f3f53db60e20a7ef685f4fa401ee8433ebf01b
AydinGhorbani <138841252+AydinGhorbani@users.noreply.github.com>
2023-10-15 03:48:41 -0400

    integrity declaration added.

--- WS5/p2/p2/SpellChecker.cpp
+++ WS5/p2/p2/SpellChecker.cpp
@@ -1,3 +1,14 @@
+/*
+****************************************
+Full Name  : Aydin Ghorbani
+Student ID#: 124170226
+Email      : aghorbani8@myseneca.ca
+
+I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+The parts that I searched or got help to do are mentioned.
+****************************************
+*/
+
 #include "SpellChecker.h"
 #include <iostream>
 #include <fstream>
