715d9e9db12db21694e2c599c135abf493ab2254
AydinGhorbani <aghorbani8@myseneca.ca>
2023-11-19 01:17:38 -0500

    Declaration Added.

--- MS2/MS2/CustomerOrder.cpp
+++ MS2/MS2/CustomerOrder.cpp
@@ -1,3 +1,14 @@
+/*
+ ****************************************
+ Full Name  : Aydin Ghorbani
+ Student ID#: 124170226
+ Email      : aghorbani8@myseneca.ca
+ Date       : 11/18/23
+ 
+ I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+ The parts that i searched or got help to do are mentioned.
+ ****************************************
+ */
 #include <iomanip>
 #include "CustomerOrder.h"
 
